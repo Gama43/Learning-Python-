345875a364253671bef5bf84a330e66db263cf4c
Gabriel Rodrigo da Gama <61113905+Gama43@users.noreply.github.com>
2021-12-02 22:29:40 -0300

    Add files via upload

---
+++ cake_recipe.py
@@ -0,0 +1,5 @@
+x, y, z = [int(w) for w in input().split()]
+
+quantidade_de_bolo = (x+y+z) / 13
+
+print('{:.0f}'.format(quantidade_de_bolo))
---
+++ how_much_cassava.py
@@ -0,0 +1,15 @@
+p_curupira = int(input())
+p_boitata = int(input())
+p_boto = int(input())
+p_mapinguari = int(input())
+p_lara = int(input())
+
+g_curupira = p_curupira * 300
+g_boitata = p_boitata * 1500
+g_boto = p_boto * 600
+g_mapinguari = p_mapinguari * 1000
+g_lara = p_lara * 150
+
+gramas = g_curupira + g_boitata + g_boto + g_lara + g_mapinguari + 225
+
+print(gramas)

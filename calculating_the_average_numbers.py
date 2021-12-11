943db608175451ff99dfbf979d683fc6ea83b18e
Gabriel Rodrigo da Gama <61113905+Gama43@users.noreply.github.com>
2021-12-10 22:49:32 -0300

    Add files via upload

---
+++ calculating_the_average_numbers.py
@@ -0,0 +1,9 @@
+tamanho = int(input())
+
+soma = 0
+for i in range(tamanho):
+    n1 = float(input())
+    soma += n1
+
+media = soma / tamanho
+print(media)
---
+++ multiples_of_n.py
@@ -0,0 +1,6 @@
+n = int(input())
+intervalo = int(input())
+
+for i in range(1, intervalo):
+    if (i%n==0):
+           print(i, end=' ')
